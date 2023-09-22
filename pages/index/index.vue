5476461e7c3583aa641e08d660900b091e620f4d
MokouQAQ <2113198@mail.nankai.edu.cn>
2023-09-22 17:48:25 +0800

    1

---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+	<button type="primary"  @click="myclick">an</button>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+			myclick(e){
+			console.log('aaa',e)
+			}
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+	.box{
+		width:100px;
+		height:100px;
+		background-color: aqua;
+	}
+	.a{
+		background-color: blue;
+	}
+</style>
